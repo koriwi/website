8d99bdf7b5135255b86fdff633307faab2e44b34
Kilian Gosewisch <kilian2798@gmail.com>
2023-03-14 14:46:11 +0100

    responsive styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./src/layouts/default.astro",
+  ],
   theme: {
+    fontFamily: {
+      mono: ["MajorMono", "monospace"],
+    },
     extend: {
+      screens: {
+        xs: "420px",
+      },
       colors: {
-        primary: "#e3381c",
+        primary: "#161616",
+      },
+      dropShadow: {
+        glowS: "0px 0px 1px rgb(255 255 255 / 60%)",
+        glowM: "0px 0px 3px rgb(255 255 255 / 90%)",
+        glowL: "0px 0px 10px rgb(255 255 255 / 60%)",
+        glowXL: "0px 0px 10px rgb(255 255 255 / 90%)",
       },
     },
   },
